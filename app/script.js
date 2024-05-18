8f5894f6fde5495d99a7310e63a9557d6b8ccc89
zuleykha <zuleykha.abdullayeva.58@mail.ru>
2024-05-18 15:38:35 +0400

    butun faylari gonder

---
+++ app/script.js
@@ -0,0 +1,21 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: true,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+      dots: true,
+    },
+    600: {
+      items: 2,
+      dots: true,
+    },
+    1000: {
+      items: 1,
+      dots: true,
+    },
+  },
+});
+
